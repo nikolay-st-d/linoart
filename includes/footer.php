266134f05dddc626c046e5eac5c7ecb9dd6b7999
Nikolay St. Dimitrov <126271396+nikolay-st-d@users.noreply.github.com>
2024-02-01 23:55:54 +0200

    layout changes

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 <div class="footer">
   <footer>
     &copy;
-    <?php echo DATE("Y"); ?> <a href="https://www.linoart.com" title="<?php echo $title; ?>">Linoart - Graphic Design
-      and Web Development Studio</a>
+    <?php echo DATE("Y"); ?> <a href="https://www.linoart.com" title="<?php echo $title; ?>">Linoart - Photos, Design
+      and Coding</a>
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-2J78SGN6S7"></script>
     <script>
--- includes/header.php
+++ includes/header.php
@@ -4,15 +4,13 @@
 				alt="Linoart visual creative studio" title="Linoart Ltd."></a>
 	</div>
 	<div class="header_right">
-		<h1>We Design!<br>We Code!</h1>
+		<h1>We Design! We Code!</h1>
 	</div>
 </div>
-
 <div class="div_nav">
 	<div class="nav_content">
 		<a href="/" title="Linoart Ltd. - home">home</a> |
-		<a href="#main" title="Linoart Ltd. - home">main</a> |
-		<a href="#profile" title="Company profile - Linoart Ltd.">company profile</a> |
+		<a href="#profile" title="Company profile - Linoart Ltd.">profile</a> |
 		<a href="#activities" title="Company activities - Linoart Ltd.">activities</a> |
 		<a href="#contact" title="Contact Linoart Ltd.">contact</a>
 	</div>
